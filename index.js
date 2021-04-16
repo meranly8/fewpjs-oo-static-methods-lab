d1c1a740111755e552974ab86a4bdc0bf14af7f1
Michelle Ranly <meranly8@gmail.com>
2021-04-15 21:41:46 -0500

    commit tree 5029ee3d2cf97f797c992035dd21eba142a01650

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    const except = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    const result = []
+    const words = string.split(" ")
+    
+    words.map((word) => {
+      if (except.includes(word)) {
+        if (result.length === 0) {
+          result.push(this.capitalize(word))
+        } else {
+          result.push(word)
+        }
+      } else {
+        result.push(this.capitalize(word))
+      }
+    })
+    return result.join(" ")
+  }
 }
